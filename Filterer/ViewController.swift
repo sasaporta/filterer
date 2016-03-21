80f3febbe01505547b3fd7c05f9cde3ec4dc605a
Steven Saporta <sasaporta@gmail.com>
2016-03-21 16:37:43 -0400

    Added constraints

--- Filterer/ViewController.swift
+++ Filterer/ViewController.swift
@@ -31,7 +31,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        imageToggle.setTitle("Show Before Image", forState: .Selected)
         
         let image = UIImage(named: "scenery")!
         
