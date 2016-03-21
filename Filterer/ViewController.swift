0ab3655534e3b42844acf5bd6c4b082c4e89a050
Steven Saporta <sasaporta@gmail.com>
2016-03-21 15:42:34 -0400

    Added ImageView

--- Filterer/ViewController.swift
+++ Filterer/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -39,7 +41,9 @@ class ViewController: UIViewController {
         }
         
         let result = rgbaImage.toUIImage()
+        print("This code has executed")
         //filteredImage = rgbaImage.toUIImage()
+        imageView.image = result
     }
 
     override func didReceiveMemoryWarning() {
